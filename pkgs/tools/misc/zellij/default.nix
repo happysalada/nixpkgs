facfe0c5b42e458b05670ca3fe29e33811f52fd4
happysalada <raphael@megzari.com>
2021-04-30 16:41:27 +0900

    zellij: 0.5.1 -> 0.6.0

--- pkgs/tools/misc/zellij/default.nix
+++ pkgs/tools/misc/zellij/default.nix
@@ -1,28 +1,22 @@
-{ lib, fetchFromGitHub, rustPlatform, stdenv, installShellFiles, libiconv }:
+{ lib, fetchFromGitHub, rustPlatform, stdenv, libiconv }:
 
 rustPlatform.buildRustPackage rec {
   pname = "zellij";
-  version = "0.5.1";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "zellij-org";
     repo = pname;
     rev = "v${version}";
-    sha256 = "102zw4napzx05rpmx6scl6il55syf3lw1gzmy1y66cg1f70sij4d";
+    sha256 = "sha256-spESDjX7scihVQrr/f6KMCI9VfdTxxPWP7FcJ965FYk=";
   };
 
-  cargoSha256 = "121fsch0an6d2hqaq0ws9cm7g5ppzfrycmmhajfacfg6wbiax1m5";
-
-  nativeBuildInputs = [ installShellFiles ];
+  cargoSha256 = "sha256-AQf9fwBKYXVtDXmVHaFrSCA51K4OQkIDL6gJyZwOo2Y=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
 
   preCheck = ''
     HOME=$TMPDIR
-  '';
-
-  postInstall = ''
-    installShellCompletion assets/completions/zellij.{bash,fish} --zsh assets/completions/_zellij
   '';
 
   meta = with lib; {
